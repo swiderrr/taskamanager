87d5b6c3baa3c8c5c04987ac7dc19feacdc9d927
Swider <swidrak.karol@gmail.com>
2022-03-27 16:58:00 +0200

    Database connected

--- tickets/settings.py
+++ tickets/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-django_heroku.setting(locals())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

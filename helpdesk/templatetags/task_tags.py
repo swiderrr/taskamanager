111d301063337bf497554c2cf7252c18d2aa9995
Swider <swidrak.karol@gmail.com>
2022-04-08 13:54:26 +0200

    Avatar color now depends on nick - function converting text to hex color

--- helpdesk/templatetags/task_tags.py
+++ helpdesk/templatetags/task_tags.py
@@ -23,3 +23,17 @@ def join_name_surname(full_name):
     joined_names = '+'.join(full_name)
     return joined_names
 
+@register.filter
+def text_to_hexcolor(full_name):
+    full_name = ''.join(full_name)
+    hex = []
+    slice = full_name[:6]
+    ascii_list = [ord(x) for x in slice]
+    for char in ascii_list:
+        num = ((char / 16) - (char // 16)) * 16
+        hex.append(int(num))
+    hex = [repr(i) for i in hex]
+    color = list(''.join(hex))
+    color = ''.join(color[:6])
+    return color
+
